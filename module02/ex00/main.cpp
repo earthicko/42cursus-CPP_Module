d259d82e4212640acd3d78a466896a040b444bab
Donghyeon Lee <earthicko@gmail.com>
2023-02-01 13:29:13 +0900

    Feat: 모듈 02 예제 00 해결

---
+++ module02/ex00/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	a.setRawBits(17110);
+	b.setRawBits(-70002666);
+	c.setRawBits(1332909);
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
+}
