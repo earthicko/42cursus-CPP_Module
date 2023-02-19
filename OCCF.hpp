09a107a623ba7099108a4a1eb1e6775c8d0bc5b4
Donghyeon Lee <earthicko@gmail.com>
2023-02-19 21:57:17 +0900

    Docs: OCCF 예시 서식

--- OCCF.hpp
+++ OCCF.hpp
@@ -1,13 +1,13 @@
 #ifndef OCCF_HPP
 # define OCCF_HPP
 
-class OCCF
+class	OCCF
 {
 	public:
-		OCCF(void);
-		OCCF(const OCCF &orig);
-		~OCCF(void);
-		OCCF &operator=(const OCCF &orig);
+				OCCF(void);
+				OCCF(const OCCF &orig);
+				~OCCF(void);
+		OCCF	&operator=(const OCCF &orig);
 };
 
 #endif
