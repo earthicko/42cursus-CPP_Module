4f94e184e4ecd3c2cb0c4ec2ef46a19e5acf5a0b
Donghyeon Lee <earthicko@gmail.com>
2023-02-21 21:10:51 +0900

    Feat: 테스트 드라이버

--- module05/ex02/main.cpp
+++ module05/ex02/main.cpp
@@ -4,7 +4,61 @@
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 
+void	testPresidentialPardonForm(void)
+{
+	Bureaucrat				choi("choi", 26);
+	Bureaucrat				lee("lee", 24);
+	Bureaucrat				kim("kim", 4);
+	PresidentialPardonForm	form("donghyle");
+	PresidentialPardonForm	form2(form);
+
+	kim.executeForm(form);
+	lee.signForm(form);
+	lee.executeForm(form);
+	kim.executeForm(form);
+	choi.signForm(form);
+	choi.executeForm(form);
+	choi.signForm(form2);
+	lee.signForm(form2);
+	kim.executeForm(form2);
+}
+
+void	testRobotomyRequestForm(void)
+{
+	Bureaucrat			choi("choi", 73);
+	Bureaucrat			lee("lee", 72);
+	Bureaucrat			kim("kim", 45);
+	RobotomyRequestForm	form("donghyle");
+
+	kim.executeForm(form);
+	lee.signForm(form);
+	lee.executeForm(form);
+	for (int i = 0; i < 5; i++)
+		kim.executeForm(form);
+	choi.signForm(form);
+	choi.executeForm(form);
+}
+
+void	testShrubberyCreationForm(void)
+{
+	Bureaucrat				choi("choi", 146);
+	Bureaucrat				lee("lee", 145);
+	Bureaucrat				kim("kim", 137);
+	ShrubberyCreationForm	form("donghyle");
+
+	kim.executeForm(form);
+	lee.signForm(form);
+	lee.executeForm(form);
+	for (int i = 0; i < 5; i++)
+		kim.executeForm(form);
+	choi.signForm(form);
+	choi.executeForm(form);
+}
+
 int	main(void)
 {
+	testPresidentialPardonForm();
+	testRobotomyRequestForm();
+	testShrubberyCreationForm();
 	return (0);
 }
