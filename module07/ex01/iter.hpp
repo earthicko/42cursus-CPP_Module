211dcf02ed9d457209d8c31b85d0c6f3b62a647f
earth <earthicko@gmail.com>
2023-03-29 00:49:54 +0900

    Feat: 모듈 07 예제 01 해결

---
+++ module07/ex01/iter.hpp
@@ -0,0 +1,27 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+# include <cstddef>
+# include <iostream>
+
+template <typename arrayElemT, typename funcT>
+void	iter(arrayElemT *arr, size_t len, funcT func)
+{
+	for (size_t i = 0; i < len; i++)
+		func(arr + i);
+}
+
+template <typename arrayElemT>
+void	printArr(arrayElemT *arr, size_t len)
+{
+	std::cout << "{";
+	for (size_t i = 0; i < len; i++)
+	{
+		std::cout << arr[i];
+		if (i < len - 1)
+			std::cout << ", ";
+	}
+	std::cout << "}";
+}
+
+#endif
---
+++ module07/ex01/main.cpp
@@ -0,0 +1,70 @@
+#include "iter.hpp"
+#include <iostream>
+
+#define LEN_ARR 10
+
+void	doubleElemInt(int *elem)
+{
+	*elem = 2 * *elem;
+}
+
+void	doubleElemLong(long *elem)
+{
+	*elem = 2 * *elem;
+}
+
+void	expElemInt(int *elem)
+{
+	*elem = 1 << *elem;
+}
+
+void	expElemLong(long *elem)
+{
+	*elem = 1 << *elem;
+}
+
+int	main(void)
+{
+	int		ints[LEN_ARR];
+	long	longs[LEN_ARR];
+
+	for (int i = 0; i < LEN_ARR; i++)
+	{
+		ints[i] = i;
+		longs[i] = i * 2;
+	}
+	std::cout << "Before doubleElemInt: ";
+	printArr(ints, LEN_ARR);
+	iter(ints, LEN_ARR, doubleElemInt);
+	std::cout << "\n";
+	std::cout << "After doubleElemInt: ";
+	printArr(ints, LEN_ARR);
+	std::cout << "\n";
+	std::cout << "Before doubleElemLong: ";
+	printArr(longs, LEN_ARR);
+	iter(longs, LEN_ARR, doubleElemLong);
+	std::cout << "\n";
+	std::cout << "After doubleElemLong: ";
+	printArr(longs, LEN_ARR);
+	std::cout << "\n";
+	for (int i = 0; i < LEN_ARR; i++)
+	{
+		ints[i] = i;
+		longs[i] = i * 2;
+	}
+	std::cout << "Before expElemInt: ";
+	printArr(ints, LEN_ARR);
+	iter(ints, LEN_ARR, expElemInt);
+	std::cout << "\n";
+	std::cout << "After expElemInt: ";
+	printArr(ints, LEN_ARR);
+	std::cout << "\n";
+	std::cout << "Before expElemLong: ";
+	printArr(longs, LEN_ARR);
+	iter(longs, LEN_ARR, expElemLong);
+	std::cout << "\n";
+	std::cout << "After expElemLong: ";
+	printArr(longs, LEN_ARR);
+	std::cout << "\n";
+	return (0);
+}
