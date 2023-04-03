cd8338c600d8c33518c8d2dd0105a84be6ff25c4
Donghyeon Lee <earthicko@gmail.com>
2023-04-03 11:42:30 +0900

    Feat: span 테스트 드라이버 보완

--- module08/ex01/main.cpp
+++ module08/ex01/main.cpp
@@ -1,9 +1,70 @@
 #include "Span.hpp"
+#include <vector>
+#include <iostream>
+
+void	getShortestSpan(const Span &span)
+{
+	try
+	{
+		int	shortestSpan;
+
+		shortestSpan = span.shortestSpan();
+		std::cout << "Shortest span: " << shortestSpan << "\n";
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << "\n";
+	}
+}
+
+void	getLongestSpan(const Span &span)
+{
+	try
+	{
+		int	longestSpan;
+
+		longestSpan = span.longestSpan();
+		std::cout << "longest span: " << longestSpan << "\n";
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << "\n";
+	}
+}
 
 int	main(void)
 {
-	Span	span(10);
+	Span				span(10);
+	std::vector<int>	twoPowered;
+	std::vector<int>	fourPowered;
 
+	for(int i = 4; i > 0; i--)
+	{
+		twoPowered.push_back((1 << i));
+		fourPowered.push_back((1 << (i * 2)));
+	}
 	span.addNumber(3);
+	getShortestSpan(span);
+	getLongestSpan(span);
+	span.addNumber(7);
+	getShortestSpan(span);
+	getLongestSpan(span);
+	span.addNumber(1);
+	getShortestSpan(span);
+	getLongestSpan(span);
+	span.insert(twoPowered.begin(), twoPowered.end());
+	getShortestSpan(span);
+	getLongestSpan(span);
+	span.addNumber(2);
+	getShortestSpan(span);
+	getLongestSpan(span);
+	try
+	{
+		span.insert(fourPowered.begin(), fourPowered.end());
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return (0);
 }
