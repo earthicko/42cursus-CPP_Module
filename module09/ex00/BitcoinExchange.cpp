f4718b7278ec467df965e939f01a36247df0b021
Donghyeon Lee <earthicko@gmail.com>
2023-04-11 12:39:02 +0900

    Feat: BitcoinExchange::load 완성

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -2,6 +2,13 @@
 #include <stdexcept>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <cstring>
+
+const std::string	BitcoinExchange::_firstColumn = "date";
+const std::string	BitcoinExchange::_secondColumn = "exchange_rate";
+const char			BitcoinExchange::_delim = ',';
+const std::string	BitcoinExchange::_dateFormat = "%Y-%m-%d";
 
 BitcoinExchange::BitcoinExchange(const std::string &filepath)
 {
@@ -23,7 +30,7 @@ BitcoinExchange	&BitcoinExchange::operator=(const BitcoinExchange &orig)
 	return (*this);
 }
 
-float	BitcoinExchange::getPrice(time_t at)
+float	BitcoinExchange::getPrice(time_t at) const
 {
 	std::cout << "Unimplemented stub of " << __func__ << "\n";
 	std::cout << "time " << at << "\n";
@@ -41,8 +48,59 @@ void	BitcoinExchange::load(const std::string &filepath)
 	lineidx = 0;
 	while (!file.eof())
 	{
-		std::string	buf;
+		std::string			line;
+		std::stringstream	buf;
+		std::string			date;
+		std::string			price;
 
-		std::getline(file, buf);
+		std::getline(file, line);
+		if (line.find(_delim) == std::string::npos)
+			break ;
+		buf.str(line);
+		std::getline(buf, date, _delim);
+		std::getline(buf, price, _delim);
+		if (lineidx == 0 && !(date == _firstColumn && price == _secondColumn))
+			throw (std::runtime_error("Invalid first line"));
+		if (lineidx > 0)
+			loadLine(date, price);
+		lineidx++;
 	}
+}
+
+static time_t	parseDate(const std::string &date, const std::string &format)
+{
+	tm		dateVal;
+	time_t	epoch;
+	char	*result;
+
+	memset(&dateVal, 0, sizeof(tm));
+	result = strptime(date.c_str(), format.c_str(), &dateVal);
+	epoch = mktime(&dateVal);
+	if (!result || epoch < 0)
+		throw (std::runtime_error(std::string("Invalid date ") + date));
+	return (epoch);
+}
+
+static float	parsePrice(const std::string &price)
+{
+	std::stringstream	buf;
+	float				priceVal;
+
+	buf.str(price);
+	buf >> priceVal;
+	if (buf.fail())
+		throw (std::runtime_error(std::string("Invalid price ") + price));
+	return (priceVal);
+}
+
+void	BitcoinExchange::loadLine(const std::string &date, const std::string &price)
+{
+	time_t	dateVal;
+	float	priceVal;
+
+	dateVal = parseDate(date, _dateFormat);
+	priceVal = parsePrice(price);
+	if (_prices.find(dateVal) != _prices.end())
+		std::cerr << "Warning: multiple price values at date " << date << "\n";
+	_prices[dateVal] = priceVal;
 }
--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -8,16 +8,21 @@
 class	BitcoinExchange
 {
 public:
-							BitcoinExchange(const std::string &filepath = "./data.csv");
-							BitcoinExchange(const BitcoinExchange &orig);
-							~BitcoinExchange(void);
-	BitcoinExchange			&operator=(const BitcoinExchange &orig);
+								BitcoinExchange(const std::string &filepath = "./data.csv");
+								BitcoinExchange(const BitcoinExchange &orig);
+								~BitcoinExchange(void);
+	BitcoinExchange				&operator=(const BitcoinExchange &orig);
 
-	float					getPrice(time_t at);
+	float						getPrice(time_t at) const;
 
 private:
-	void					load(const std::string &filepath);
-	std::map<time_t, float>	_prices;
+	void						load(const std::string &filepath);
+	void						loadLine(const std::string &date, const std::string &price);
+	static const std::string	_firstColumn;
+	static const std::string	_secondColumn;
+	static const char			_delim;
+	static const std::string	_dateFormat;
+	std::map<time_t, float>		_prices;
 };
 
 #endif
