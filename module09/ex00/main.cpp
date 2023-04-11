4cae61f8879f44b6d2e34d00e25438f56bf73131
Donghyeon Lee <earthicko@gmail.com>
2023-04-11 20:57:33 +0900

    Feat: 각 줄 파싱 및 쿼리 과정 구현

--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -1,11 +1,55 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+void	printPriceAt(const BitcoinExchange &bx, const std::string &date, const std::string &amount)
+{
+	time_t	dateVal;
+	float	amountVal;
+	float	price;
+
+	dateVal = bx.parseDate(date);
+	amountVal = bx.parseNumber(amount);
+	price = bx.getPrice(dateVal);
+}
+
+void	processLine(const BitcoinExchange &bx, std::ifstream &file)
+{
+	std::string			line;
+	std::stringstream	buf;
+	std::string			first;
+	std::string			delim;
+	std::string			second;
+
+	std::getline(file, line);
+	buf.str(line);
+	buf >> first;
+	buf >> delim;
+	buf >> second;
+	if (delim != "|" || first.length() == 0 || second.length() == 0)
+		throw (std::runtime_error(std::string("Invalid line ") + line));
+	printPriceAt(bx, first, second);
+}
 
 void	process(const BitcoinExchange &bx, const std::string &filepath)
 {
-	std::cout << "Unimplemented stub of " << __func__ << "\n";
-	(void)bx;
-	(void)filepath;
+	std::ifstream	file;
+
+	file.open(filepath, std::ios_base::in);
+	if (!file.is_open())
+		throw (std::ifstream::failure(std::string("Failed to open ") + filepath));
+	while (!file.eof())
+	{
+		try
+		{
+			processLine(bx, file);
+		}
+		catch (const std::runtime_error &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
 }
 
 int	main(int argc, char **argv)
